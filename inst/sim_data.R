09eff93815761bf029c2ba24bd9ab22ef2a72939
Justin Lee <jjlee13@illinois.edu>
2017-06-02 02:43:30 -0500

    add inst/ + travis CI try #8

---
+++ inst/sim_data.R
@@ -0,0 +1,32 @@
+## ---- echo = FALSE-------------------------------------------------------
+library(simts)
+
+## ------------------------------------------------------------------------
+# Load hydro dataset
+data("hydro")
+# Simulate based on data
+hydro = gts(as.vector(hydro), start = 1907, freq = 12, unit_ts = "in.", 
+            name_ts = "Precipitation", data_name = "Hydrology data")
+
+# Load savingrt dataset
+data("savingrt")
+# Simulate based on data
+savingrt = gts(as.vector(savingrt), start = 1959, freq = 12, unit_ts = "%", 
+            name_ts = "Saving Rates", data_name = "US Personal Saving Rates")
+
+# Load Nile dataset
+Nile = datasets::Nile
+nile = gts(as.vector(Nile), start = 1871, end = 1970, freq = 1, 
+           unit_ts = bquote(paste(10^8," ",m^3)), name_ts = "Flow", 
+           name_time = "Years", data_name = "Annual Flow of river Nile")
+
+## ---- fig.align='center', fig.height = 5, fig.width = 7.25---------------
+# Plot hydro simulation
+plot(hydro)
+
+# Plot savingrt simulation
+plot(savingrt)
+
+# Plot Nile simulation 
+plot(nile)
+

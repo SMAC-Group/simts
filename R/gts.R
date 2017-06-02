178a910e5dfb9cd5018fb53e7ed851d3fc9d9a54
Justin Lee <jjlee13@illinois.edu>
2017-06-01 21:53:00 -0500

    change par(mar) to fit in expressions for y-axis

--- R/gts.R
+++ R/gts.R
@@ -369,7 +369,7 @@ plot.gts = function(x, xlab = NULL, ylab = NULL, main = NULL, couleur = "blue4",
     end = scales[n_x]
   }
   
-  par(mar = c(5.1, 4.1, 1, 2.1))
+  par(mar = c(5.1, 5.1, 1, 2.1))
   
   # Main plot                     
   plot(NA, xlim = c(start, end), ylim = range(x), xlab = name_time, ylab = name_ts, 

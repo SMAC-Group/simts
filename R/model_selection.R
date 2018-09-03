5bcff019767b8cb762d9eba7fb69ece9a621c74f
Yuming Zhang <yzhan216@illinois.edu>
2018-09-03 10:17:32 -0400

    minor update for plot select_ar and select_ma

--- R/model_selection.R
+++ R/model_selection.R
@@ -153,6 +153,194 @@ best_model = function(x, ic = "aic"){
 # Plotting Functions
 ##########################
 
+# ----- For select_ar
+
+plot_select_ar = function(x, main){
+  # Labs and title
+  xlab = "Autoregressive (p)"
+  ylab = "Criterion Values"
+  
+  if(is.null(main)){
+    main = "Model Selection for Autoregressive Process"
+  }else{
+    main = main
+  }
+  
+  # Store values to prepare for plotting
+  p_max = max(x$p)
+  p_min = min(x$p)
+  p_length = p_max - p_min + 1
+  aic_value = rep(NA, p_length)
+  bic_value = rep(NA, p_length)
+  hq_value = rep(NA, p_length)
+  
+  for (i in 1:p_length){
+    aic_value[i] = x$value[i]
+    bic_value[i] = x$value[i+p_length]
+    hq_value[i] = x$value[i+2*p_length]
+  }
+  
+  
+  # Main plot
+  plot(NA, xlim = c(p_min, p_max), ylim = range(x$value), 
+       xlab = xlab, ylab = ylab, 
+       xaxt = 'n', yaxt = 'n', bty = "n", ann = FALSE)
+  win_dim = par("usr")
+  
+  par(new = TRUE)
+  plot(NA, xlim = c(p_min, p_max), ylim = c(win_dim[3], win_dim[4] + 0.09*(win_dim[4] - win_dim[3])),
+       xlab = xlab, ylab = ylab, xaxt = 'n', yaxt = 'n', bty = "n")
+  win_dim = par("usr")
+  
+  # Add grid
+  grid(NULL, NULL, lty = 1, col = "grey95")
+  
+  
+  # Add title
+  x_vec = c(win_dim[1], win_dim[2], win_dim[2], win_dim[1])
+  y_vec = c(win_dim[4], win_dim[4],
+            win_dim[4] - 0.09*(win_dim[4] - win_dim[3]),
+            win_dim[4] - 0.09*(win_dim[4] - win_dim[3]))
+  polygon(x_vec, y_vec, col = "grey95", border = NA)
+  text(x = mean(c(win_dim[1], win_dim[2])),
+       y = (win_dim[4] - 0.09/2*(win_dim[4] - win_dim[3])), 
+       main)
+  
+  # Add axes and box
+  lines(x_vec[1:2], rep((win_dim[4] - 0.09*(win_dim[4] - win_dim[3])),2), col = 1)
+  box()
+  axis(1, at = seq(p_min, p_max), padj = 0.3)
+  y_axis = axis(2, labels = FALSE, tick = FALSE)
+  y_axis = y_axis[y_axis < (win_dim[4] - 0.09*(win_dim[4] - win_dim[3]))]
+  axis(2, padj = -0.2, at = y_axis)
+  
+  # Plotting
+  col_aic = "#F8766DFF"
+  col_bic = "#00BA38FF"
+  col_hq = "#619CFFFF"
+  lines(seq(p_min, p_max), aic_value, col = col_aic, lwd = 2)
+  lines(seq(p_min, p_max), bic_value, col = col_bic, lwd = 2)
+  lines(seq(p_min, p_max), hq_value, col = col_hq, lwd = 2)
+  
+  # Add best models
+  points(x$p[which.min(aic_value)], aic_value[which.min(aic_value)], 
+         col = col_aic, pch = 16, cex = 2)
+  points(x$p[which.min(bic_value)], bic_value[which.min(bic_value)], 
+         col = col_bic, pch = 16, cex = 2)
+  points(x$p[which.min(hq_value)], hq_value[which.min(hq_value)], 
+         col = col_hq, pch = 16, cex = 2)
+  
+  # Add legend
+  usr = par("usr")
+  lgd = legend(x = mean(c(usr[1],usr[2])), 
+               y =  mean(c(usr[3],usr[4])),
+               plot = F,
+               legend = c("AIC", "BIC", "HQ"))
+  legend(x = usr[1] + lgd$rect$w*0.4,
+         y =  usr[4] - lgd$rect$h*0.3,
+         legend = c("AIC", "BIC", "HQ"), 
+         text.col = rep("black", 3),
+         lty = rep(1,3),
+         pch = rep(16,3),
+         col = c(col_aic, col_bic, col_hq),
+         bty = "n")
+}
+
+
+
+plot_select_ma = function(x, main){
+  # Labs and title
+  xlab = "Moving Average (q)"
+  ylab = "Criterion Values"
+  
+  if(is.null(main)){
+    main = "Model Selection for Moving Average Process"
+  }else{
+    main = main
+  }
+  
+  # Store values to prepare for plotting
+  q_max = max(x$q)
+  q_min = min(x$q)
+  q_length = q_max - q_min + 1
+  aic_value = rep(NA, q_length)
+  bic_value = rep(NA, q_length)
+  hq_value = rep(NA, q_length)
+  
+  for (i in 1:q_length){
+    aic_value[i] = x$value[i]
+    bic_value[i] = x$value[i+q_length]
+    hq_value[i] = x$value[i+2*q_length]
+  }
+  
+  
+  # Main plot
+  plot(NA, xlim = c(q_min, q_max), ylim = range(x$value), 
+       xlab = xlab, ylab = ylab, 
+       xaxt = 'n', yaxt = 'n', bty = "n", ann = FALSE)
+  win_dim = par("usr")
+  
+  par(new = TRUE)
+  plot(NA, xlim = c(q_min, q_max), ylim = c(win_dim[3], win_dim[4] + 0.09*(win_dim[4] - win_dim[3])),
+       xlab = xlab, ylab = ylab, xaxt = 'n', yaxt = 'n', bty = "n")
+  win_dim = par("usr")
+  
+  # Add grid
+  grid(NULL, NULL, lty = 1, col = "grey95")
+  
+  
+  # Add title
+  x_vec = c(win_dim[1], win_dim[2], win_dim[2], win_dim[1])
+  y_vec = c(win_dim[4], win_dim[4],
+            win_dim[4] - 0.09*(win_dim[4] - win_dim[3]),
+            win_dim[4] - 0.09*(win_dim[4] - win_dim[3]))
+  polygon(x_vec, y_vec, col = "grey95", border = NA)
+  text(x = mean(c(win_dim[1], win_dim[2])),
+       y = (win_dim[4] - 0.09/2*(win_dim[4] - win_dim[3])), 
+       main)
+  
+  # Add axes and box
+  lines(x_vec[1:2], rep((win_dim[4] - 0.09*(win_dim[4] - win_dim[3])),2), col = 1)
+  box()
+  axis(1, at = seq(q_min, q_max), padj = 0.3)
+  y_axis = axis(2, labels = FALSE, tick = FALSE)
+  y_axis = y_axis[y_axis < (win_dim[4] - 0.09*(win_dim[4] - win_dim[3]))]
+  axis(2, padj = -0.2, at = y_axis)
+  
+  
+  # Plotting
+  col_aic = "#F8766DFF"
+  col_bic = "#00BA38FF"
+  col_hq = "#619CFFFF"
+  lines(seq(q_min, q_max), aic_value, col = col_aic, lwd = 2)
+  lines(seq(q_min, q_max), bic_value, col = col_bic, lwd = 2)
+  lines(seq(q_min, q_max), hq_value, col = col_hq, lwd = 2)
+  
+  # Add best models
+  points(x$q[which.min(aic_value)], aic_value[which.min(aic_value)], 
+         col = col_aic, pch = 16, cex = 2)
+  points(x$q[which.min(bic_value)], bic_value[which.min(bic_value)], 
+         col = col_bic, pch = 16, cex = 2)
+  points(x$q[which.min(hq_value)], hq_value[which.min(hq_value)], 
+         col = col_hq, pch = 16, cex = 2)
+  
+  # Add legend
+  usr = par("usr")
+  lgd = legend(x = mean(c(usr[1],usr[2])), 
+               y =  mean(c(usr[3],usr[4])),
+               plot = F,
+               legend = c("AIC", "BIC", "HQ"))
+  legend(x = usr[1] + lgd$rect$w*0.4,
+         y =  usr[4] - lgd$rect$h*0.3,
+         legend = c("AIC", "BIC", "HQ"), 
+         text.col = rep("black", 3),
+         lty = rep(1,3),
+         pch = rep(16,3),
+         col = c(col_aic, col_bic, col_hq),
+         bty = "n")
+}
+
+
 #' @title Visualization of Model Selection 
 #' @description This function visualize the model comparison based on different model selection criteria. 
 #' @param x An object that is either of type \code{\link{select_ar}}
@@ -179,192 +367,13 @@ plot.select_arima = function(x, main = NULL){
   }
   
   # ----- for select_ar
-  
   if ("select_ar" %in% class(x) & !"select_ma" %in% class(x)){
-    # Labs and title
-    xlab = "Autoregressive (p)"
-    ylab = "Criterion Values"
-    
-    if(is.null(main)){
-      main = "Model Selection for Autoregressive Process"
-    }else{
-      main = main
-    }
-    
-    # Store values to prepare for plotting
-    p_max = max(x$p)
-    p_min = min(x$p)
-    p_length = p_max - p_min + 1
-    aic_value = rep(NA, p_length)
-    bic_value = rep(NA, p_length)
-    hq_value = rep(NA, p_length)
-    
-    for (i in 1:p_length){
-      aic_value[i] = x$value[i]
-      bic_value[i] = x$value[i+p_length]
-      hq_value[i] = x$value[i+2*p_length]
-    }
-    
-    
-    # Main plot
-    plot(NA, xlim = c(p_min, p_max), ylim = range(x$value), 
-         xlab = xlab, ylab = ylab, 
-         xaxt = 'n', yaxt = 'n', bty = "n", ann = FALSE)
-    win_dim = par("usr")
-    
-    par(new = TRUE)
-    plot(NA, xlim = c(p_min, p_max), ylim = c(win_dim[3], win_dim[4] + 0.09*(win_dim[4] - win_dim[3])),
-         xlab = xlab, ylab = ylab, xaxt = 'n', yaxt = 'n', bty = "n")
-    win_dim = par("usr")
-    
-    # Add grid
-    grid(NULL, NULL, lty = 1, col = "grey95")
-    
-    
-    # Add title
-    x_vec = c(win_dim[1], win_dim[2], win_dim[2], win_dim[1])
-    y_vec = c(win_dim[4], win_dim[4],
-              win_dim[4] - 0.09*(win_dim[4] - win_dim[3]),
-              win_dim[4] - 0.09*(win_dim[4] - win_dim[3]))
-    polygon(x_vec, y_vec, col = "grey95", border = NA)
-    text(x = mean(c(win_dim[1], win_dim[2])),
-         y = (win_dim[4] - 0.09/2*(win_dim[4] - win_dim[3])), 
-         main)
-    
-    # Add axes and box
-    lines(x_vec[1:2], rep((win_dim[4] - 0.09*(win_dim[4] - win_dim[3])),2), col = 1)
-    box()
-    axis(1, at = seq(p_min, p_max), padj = 0.3)
-    y_axis = axis(2, labels = FALSE, tick = FALSE)
-    y_axis = y_axis[y_axis < (win_dim[4] - 0.09*(win_dim[4] - win_dim[3]))]
-    axis(2, padj = -0.2, at = y_axis)
-    
-    
-    
-    # Plotting
-    col_aic = "#F8766DFF"
-    col_bic = "#00BA38FF"
-    col_hq = "#619CFFFF"
-    lines(seq(p_min, p_max), aic_value, col = col_aic, lwd = 2)
-    lines(seq(p_min, p_max), bic_value, col = col_bic, lwd = 2)
-    lines(seq(p_min, p_max), hq_value, col = col_hq, lwd = 2)
-    
-    # Add best models
-    points(x$p[which.min(aic_value)], aic_value[which.min(aic_value)], 
-           col = col_aic, pch = 16, cex = 2)
-    points(x$p[which.min(bic_value)], bic_value[which.min(bic_value)], 
-           col = col_bic, pch = 16, cex = 2)
-    points(x$p[which.min(hq_value)], hq_value[which.min(hq_value)], 
-           col = col_hq, pch = 16, cex = 2)
-    
-    # Add legend
-    usr = par("usr")
-    lgd = legend(x = mean(c(usr[1],usr[2])), 
-                 y =  mean(c(usr[3],usr[4])),
-                 plot = F,
-                 legend = c("AIC", "BIC", "HQ"))
-    legend(x = usr[1] + lgd$rect$w*0.4,
-           y =  usr[4] - lgd$rect$h*0.3,
-           legend = c("AIC", "BIC", "HQ"), 
-           text.col = rep("black", 3),
-           lty = rep(1,3),
-           pch = rep(16,3),
-           col = c(col_aic, col_bic, col_hq),
-           bty = "n")
+    plot_select_ar(x=x, main=main)
   }
   
   # ----- for select_ma
-  
   if ("select_ma" %in% class(x) & !"select_ar" %in% class(x)){
-    # Labs and title
-    xlab = "Moving Average (q)"
-    ylab = "Criterion Values"
-    
-    if(is.null(main)){
-      main = "Model Selection for Moving Average Process"
-    }else{
-      main = main
-    }
-    
-    # Store values to prepare for plotting
-    q_max = max(x$q)
-    q_min = min(x$q)
-    q_length = q_max - q_min + 1
-    aic_value = rep(NA, q_length)
-    bic_value = rep(NA, q_length)
-    hq_value = rep(NA, q_length)
-    
-    for (i in 1:q_length){
-      aic_value[i] = x$value[i]
-      bic_value[i] = x$value[i+q_length]
-      hq_value[i] = x$value[i+2*q_length]
-    }
-    
-    
-    # Main plot
-    plot(NA, xlim = c(q_min, q_max), ylim = range(x$value), 
-         xlab = xlab, ylab = ylab, 
-         xaxt = 'n', yaxt = 'n', bty = "n", ann = FALSE)
-    win_dim = par("usr")
-    
-    par(new = TRUE)
-    plot(NA, xlim = c(q_min, q_max), ylim = c(win_dim[3], win_dim[4] + 0.09*(win_dim[4] - win_dim[3])),
-         xlab = xlab, ylab = ylab, xaxt = 'n', yaxt = 'n', bty = "n")
-    win_dim = par("usr")
-    
-    # Add grid
-    grid(NULL, NULL, lty = 1, col = "grey95")
-    
-    
-    # Add title
-    x_vec = c(win_dim[1], win_dim[2], win_dim[2], win_dim[1])
-    y_vec = c(win_dim[4], win_dim[4],
-              win_dim[4] - 0.09*(win_dim[4] - win_dim[3]),
-              win_dim[4] - 0.09*(win_dim[4] - win_dim[3]))
-    polygon(x_vec, y_vec, col = "grey95", border = NA)
-    text(x = mean(c(win_dim[1], win_dim[2])),
-         y = (win_dim[4] - 0.09/2*(win_dim[4] - win_dim[3])), 
-         main)
-    
-    # Add axes and box
-    lines(x_vec[1:2], rep((win_dim[4] - 0.09*(win_dim[4] - win_dim[3])),2), col = 1)
-    box()
-    axis(1, at = seq(q_min, q_max), padj = 0.3)
-    y_axis = axis(2, labels = FALSE, tick = FALSE)
-    y_axis = y_axis[y_axis < (win_dim[4] - 0.09*(win_dim[4] - win_dim[3]))]
-    axis(2, padj = -0.2, at = y_axis)
-    
-    
-    # Plotting
-    col_aic = "#F8766DFF"
-    col_bic = "#00BA38FF"
-    col_hq = "#619CFFFF"
-    lines(seq(q_min, q_max), aic_value, col = col_aic, lwd = 2)
-    lines(seq(q_min, q_max), bic_value, col = col_bic, lwd = 2)
-    lines(seq(q_min, q_max), hq_value, col = col_hq, lwd = 2)
-    
-    # Add best models
-    points(x$q[which.min(aic_value)], aic_value[which.min(aic_value)], 
-           col = col_aic, pch = 16, cex = 2)
-    points(x$q[which.min(bic_value)], bic_value[which.min(bic_value)], 
-           col = col_bic, pch = 16, cex = 2)
-    points(x$q[which.min(hq_value)], hq_value[which.min(hq_value)], 
-           col = col_hq, pch = 16, cex = 2)
-    
-    # Add legend
-    usr = par("usr")
-    lgd = legend(x = mean(c(usr[1],usr[2])), 
-                 y =  mean(c(usr[3],usr[4])),
-                 plot = F,
-                 legend = c("AIC", "BIC", "HQ"))
-    legend(x = usr[1] + lgd$rect$w*0.4,
-           y =  usr[4] - lgd$rect$h*0.3,
-           legend = c("AIC", "BIC", "HQ"), 
-           text.col = rep("black", 3),
-           lty = rep(1,3),
-           pch = rep(16,3),
-           col = c(col_aic, col_bic, col_hq),
-           bty = "n")
+    plot_select_ma(x=x, main=main)
   }
   
 }

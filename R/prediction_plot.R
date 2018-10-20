e03b764481734961d82473bd1f5396afb5e4807a
Yuming Zhang <yzhan216@illinois.edu>
2018-10-20 16:32:36 -0400

    fix plot_pred, remove time.pred input and when Time is numeric

--- R/prediction_plot.R
+++ R/prediction_plot.R
@@ -23,7 +23,7 @@
 #' @param x A \code{gts} object
 #' @param model A \code{ts} model
 #' @param n.ahead An \code{integer} indicating number of units of time ahead for which to make forecasts
-#' @param time.pred A \code{vector} indicating the time points for which the forecasts are made (\code{lenght(vector)} must be equal to \code{n.ahead})
+#' @param level A \code{double} indicating confidence level of prediction interval. 
 #' @param xlab A \code{string} for the title of x axis
 #' @param ylab A \code{string} for the title of y axis
 #' @param main A \code{string} for the over all title of the plot
@@ -31,16 +31,27 @@
 #' @author Yuming Zhang
 #' @export
 #' @examples
+#' # Example where Time is null
 #' x = gts(as.vector(lynx), start = 1821, end = 1934, freq = 1, 
 #' unit_ts = bquote(paste(10^8," ",m^3)), name_ts = "Numbers", 
 #' unit_time = "year", data_name = "Annual Numbers of Lynx Trappings")
 #' 
 #' model = arima(x, c(2,0,0))
 #' n.ahead = 20
-#' time.pred = 1935:1954
-#' plot_pred(x, model, n.ahead, time.pred)
+#' plot_pred(x, model, n.ahead)
+#' plot_pred(x, model, n.ahead, level = 0.90)
+#' 
+#' # Example where Time is not numeric
+#' Time = c("2018-08-30", "2018-08-31", "2018-09-01", "2018-09-02", "2018-09-03", 
+#' "2018-09-04", "2018-09-05", "2018-09-06", "2018-09-07", "2018-09-08")
+#' x = gen_ar1(10, 0.3, 1)
+#' x = gts(x, freq = 1, Time = Time)
+#' model= arima(x, c(1,0,0))
+#' n.ahead = 4
+#' plot_pred(x, model, n.ahead)
 
-plot_pred = function(x, model, n.ahead, time.pred,
+
+plot_pred = function(x, model, n.ahead, level = 0.95, 
                      xlab = NULL, ylab = NULL, main = NULL, ...){
   
   # Extract values
@@ -60,29 +71,23 @@ plot_pred = function(x, model, n.ahead, time.pred,
   # Warning 
   if (n_x == 0){stop('Time series is empty!')}
   if(!is(x,"gts")){stop('Object must be a gts object. Use functions gts() or gen_gts() to create it.')}
-  if (length(time.pred) != n.ahead){stop('Number of required forecasts (n.ahead) do not correspond to given time points (time.pred)')}
+  # if (length(time.pred) != n.ahead){stop('Number of required forecasts (n.ahead) do not correspond to given time points (time.pred)')}
   
   # Prediction 
   prediction = predict(model, n.ahead = n.ahead)
   pred = prediction$pred
   se = prediction$se
+  ci.up = pred+qnorm(level)*se
+  ci.low = pred-qnorm(level)*se
   
   # Labels
-  if (!is.null(xlab)){
-    name_time = xlab
-  }
+  if (!is.null(xlab)){ name_time = xlab }
   
-  if (!is.null(ylab)){
-    name_ts = ylab
-  }
+  if (!is.null(ylab)){ name_ts = ylab }
   
-  if (is.null(name_time)){
-    name_time = "Time"
-  }
+  if (is.null(name_time)){ name_time = "Time" }
   
-  if (is.null(name_ts)){
-    name_ts = "Observation and Prediction"
-  }
+  if (is.null(name_ts)){ name_ts = "Observation and Prediction" }
   
   if (!is.null(unit_time)){
     if (class(unit_time) == "name" || class(unit_time) == "call"){
@@ -121,61 +126,81 @@ plot_pred = function(x, model, n.ahead, time.pred,
     end = scales[n_x]
   }
   
+  
   if (is.null(Time)){
+    # time pred
+    time.pred = seq(end+1, end + floor(n.ahead / freq))
+    scale.pred = c(end, time.pred)
+    
     # Make frame
     make_frame(x_range = range(c(scales, time.pred)), 
-               y_range = range(c(x,pred), na.rm = TRUE), 
+               y_range = range(c(x,pred, ci.up, ci.low), 
+                               na.rm = TRUE), 
                xlab = name_time, ylab = name_ts, main = main) 
     
     # Add lines 
     couleur = "blue4"
     lines(scales, x, type = "l", col = couleur, lty = 1)
     
-    scales_pred = c(end, time.pred)
-    lines(scales_pred, c(x[n_x], pred), type = "l", col = couleur, lty = 2)
+    lines(scale.pred, c(x[n_x], pred), type = "l", col = couleur, lty = 2)
     
     # Add CI
-    polygon(x = c(scales_pred, rev(scales_pred)), 
-            y = c(x[n_x], pred-2*se, rev(c(x[n_x], pred+2*se))), 
-            col = rgb(0,0.1,1,0.1), border = NA)
-    polygon(x = c(scales_pred, rev(scales_pred)), 
-            y = c(x[n_x], pred-se, rev(c(x[n_x], pred+se))), 
+    polygon(x = c(scale.pred, rev(scale.pred)), 
+            y = c(x[n_x], ci.low, rev(c(x[n_x], ci.up))), 
             col = rgb(0,0.1,1,0.1), border = NA)
   }
   
   else {
     if(!is.numeric(Time)){
+      # time.pred
+      Time_int = as.integer(as.Date(Time, origin="1970-01-01")) # 1970-01-01 is default origin
+      start = Time_int[1]
+      end = Time_int[length(Time)]
+      time.pred = end + seq(1, floor(n.ahead / freq))
+
       # Make frame
-      make_frame(x_range = range(c(Time, time.pred)), 
-                 y_range = range(c(x,pred), na.rm = TRUE), 
+      make_frame(x_range = range(c(Time_int, time.pred)), 
+                 y_range = range(c(x,pred, ci.low, ci.up), na.rm = TRUE), 
                  add_axis_x = FALSE, 
                  xlab = name_time, ylab = name_ts, main = main)
       
       # Add x axis
-      axis.Date(1, c(Time, time.pred))
+      axis.Date(1, as.Date(c(Time_int, time.pred), , origin="1970-01-01") )
+      
+      # Add lines
+      couleur = "blue4"
+      lines(Time_int, x, type = "l", col = couleur, lty = 1)
+      
+      scales.pred = c(end, time.pred)
+      lines(scales.pred, c(x[n_x], pred), type = "l", col = couleur, lty = 2)
+      
+      # Add CI
+      polygon(x = c(scales.pred, rev(scales.pred)), 
+              y = c(x[n_x], ci.low, rev(c(x[n_x], ci.up))), 
+              col = rgb(0,0.1,1,0.1), border = NA)
     }
     else {
+      # time.pred
+      time.pred = end + seq(1, floor(n.ahead / freq))
+      
       # Make frame
       make_frame(x_range = range(c(Time, time.pred)), 
-                 y_range = range(c(x,pred), na.rm = TRUE), 
+                 y_range = range(c(x,pred, ci.low, ci.up), na.rm = TRUE), 
                  xlab = name_time, ylab = name_ts, main = main) 
       
+      # Add lines
+      couleur = "blue4"
+      lines(Time, x, type = "l", col = couleur, lty = 1)
+      
+      scales.pred = c(end, time.pred)
+      lines(scales.pred, c(x[n_x], pred), type = "l", col = couleur, lty = 2)
+      
+      # Add CI
+      polygon(x = c(scales.pred, rev(scales.pred)), 
+              y = c(x[n_x], ci.low, rev(c(x[n_x], ci.up))), 
+              col = rgb(0,0.1,1,0.1), border = NA)
+      
     }
-    
-    # Add lines
-    couleur = "blue4"
-    lines(Time, x, type = "l", col = couleur, lty = 1)
-    
-    scales_pred = c(Time[n_x], time.pred)
-    lines(scales_pred, c(x[n_x], pred), type = "l", col = couleur, lty = 2)
-    
-    # Add CI
-    polygon(x = c(scales_pred, rev(scales_pred)), 
-            y = c(x[n_x], pred-2*se, rev(c(x[n_x], pred+2*se))), 
-            col = rgb(0,0.1,1,0.1), border = NA)
-    polygon(x = c(scales_pred, rev(scales_pred)), 
-            y = c(x[n_x], pred-se, rev(c(x[n_x], pred+se))), 
-            col = rgb(0,0.1,1,0.1), border = NA)
     
   }
 }
